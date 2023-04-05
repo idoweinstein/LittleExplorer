8ef147fd5eda3970bded7784171c60e4f85b0437
Almog Altman <almogaltman1@mail.tau.ac.il>
2023-04-05 10:02:01 +0300

    add full text index to name

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: LittleExplorer
 -- ------------------------------------------------------
@@ -16,13 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Kindergarten`
+-- Table structure for table `kindergarten`
 --
 
-DROP TABLE IF EXISTS `Kindergarten`;
+DROP TABLE IF EXISTS `kindergarten`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Kindergarten` (
+CREATE TABLE `kindergarten` (
   `kindergarten_id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `address` varchar(1000) NOT NULL,
@@ -37,18 +37,19 @@ CREATE TABLE `Kindergarten` (
   `has_zaharon` tinyint unsigned NOT NULL,
   `has_parking` tinyint unsigned NOT NULL,
   PRIMARY KEY (`kindergarten_id`),
-  UNIQUE KEY `kindergarten_id_UNIQUE` (`kindergarten_id`)
+  UNIQUE KEY `kindergarten_id_UNIQUE` (`kindergarten_id`),
+  FULLTEXT KEY `idx_kindergarten_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Kindergarten`
+-- Dumping data for table `kindergarten`
 --
 
-LOCK TABLES `Kindergarten` WRITE;
-/*!40000 ALTER TABLE `Kindergarten` DISABLE KEYS */;
-INSERT INTO `Kindergarten` VALUES (1,'פעוטון זנבנבון','האורניום 6, דימונה','דימונה',3,36,20,10,5,'07:00:00','16:00:00',0,1);
-/*!40000 ALTER TABLE `Kindergarten` ENABLE KEYS */;
+LOCK TABLES `kindergarten` WRITE;
+/*!40000 ALTER TABLE `kindergarten` DISABLE KEYS */;
+INSERT INTO `kindergarten` VALUES (1,'פעוטון זנבנבון','האורניום 6, דימונה','דימונה',3,36,20,10,5,'07:00:00','16:00:00',0,1);
+/*!40000 ALTER TABLE `kindergarten` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -60,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-03 11:51:12
+-- Dump completed on 2023-04-05  9:57:56
