382d3a399bea7ef7de7a58e573d107c64139b41b
djaffe <danieljaffe@mail.tau.ac.il>
2023-04-12 15:18:27 +0300

    1. Added backend for main page 2. No more app/ in all requests

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('kindergartens/', views.get_kindergarten, name='kindergartens')
+    path('kindergartens/', views.get_kindergarten, name='kindergartens'),
+    path('', views.index, name='mainPage')
 ]
