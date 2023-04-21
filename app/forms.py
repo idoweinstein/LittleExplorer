00e43aff07debbfc90b145e4e9b7e0c820956dab
djaffe <danieljaffe@mail.tau.ac.il>
2023-04-21 12:55:50 +0300

    adjusted user registration with the following adjustments: 1. removed name from parent model 2. added first_name and last_name fields to registration form 3. username == email after registration

--- app/forms.py
+++ app/forms.py
@@ -12,7 +12,17 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = Parent
         fields = fields = (
-            'email', 'home_address', 'home_region', 'work_address', 'work_region', 'password1', 'password2')
+            'email', 'first_name', 'last_name', 'home_address', 'home_region', 'work_address', 'work_region',
+            'password1',
+            'password2')
+
+    def save(self, commit=True):
+        user = super(RegisterUserForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.username = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
@@ -21,6 +31,8 @@ class RegisterUserForm(UserCreationForm):
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
 
+        self.fields['first_name'].required = False
+        self.fields['last_name'].required = False
         self.fields['home_address'].required = False
         self.fields['home_region'].required = False
         self.fields['work_address'].required = False
