73313e25fae7dcdc0ac03a400536887aa0545c4e
djaffe <danieljaffe@mail.tau.ac.il>
2023-04-22 20:19:30 +0300

    Fix parent table

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-from .models import Kindergarten, Kindergartenadditionalinfo
+from .models import Kindergarten, Kindergartenadditionalinfo, Parent
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegisterUserForm
 from django.contrib import messages
 from django.contrib.auth import login, authenticate
+import pandas as pd
 
 
 def index(request):
@@ -48,4 +49,24 @@ def sign_up(request):
 def get_kindergarten_details(request, kindergarten_id):
     kindergarten = get_object_or_404(Kindergarten, pk=kindergarten_id)
     kindergarten_info = get_object_or_404(Kindergartenadditionalinfo, pk=kindergarten_id)
-    return render(request, 'kindergarten.html',{'kindergarten': kindergarten, 'kindergarten_info' : kindergarten_info})
+    return render(request, 'kindergarten.html', {'kindergarten': kindergarten, 'kindergarten_info': kindergarten_info})
+
+
+def fix_parent(request):
+    df = pd.read_csv('/home/djaffe/PycharmProjects/LittleExplorer/data/parent.csv')
+    df = df.reset_index()
+    for index, row in df.iterrows():
+        email = row["user_name"].lstrip()
+        password = row["password"].lstrip()
+        first_name = row["first_name"].lstrip()
+        last_name = row["last_name"].lstrip()
+        home_address = row["home_address"].lstrip()
+        home_region = row["home_region"].lstrip()
+        work_address = row["work_address"].lstrip()
+        work_region = row["work_region"].lstrip()
+        user = Parent.objects.create_user(email=email, username=email, password=password, first_name=first_name,
+                                          last_name=last_name, home_address=home_address, home_region=home_region,
+                                          work_address=work_address, work_region=work_region)
+        if user is not None:
+            login(request, user)
+    return render(request, 'index.html')
